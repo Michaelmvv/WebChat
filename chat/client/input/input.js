ab4b4737e68e449c7b03bb7375b85061b81d75ad
Michaelmvv <bukkit.michaelmvv@gmail.com>
2015-06-09 18:00:46 -0700

    More stuffs!!

--- chat/client/input/input.js
+++ chat/client/input/input.js
@@ -2,7 +2,18 @@ Template.input.events = {
 	'submit form': function(event){
 		event.preventDefault();
 		if (Meteor.user()){
-			var name = Meteor.user().emails[0].address; //Meteor.user().profile.name;
+			
+			//	var name = Meteor.user().emails[0].address; //Meteor.user().profile.name;
+			try {
+			var name = Meteor.user().profile.name;
+			} catch(err) {
+			try{
+			var name = Meteor.user().emails[0].address;
+			}catch(err){
+			console.log("Could not find profile name type.... Oh No!");
+			}
+			} 
+		
 		} else {
 			var name = 'Anonymous';
 		}
@@ -18,6 +29,7 @@ Template.input.events = {
 
 		document.getElementById('message').value = '';
 		message.value = '';
+		console.log("Message Sent");
 		};
 	} 
 } 
